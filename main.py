299146c7247b1c67fed518511a9e2535a5c9fd32
pgibb96 <pgibb96@gmail.com>
2020-08-03 07:09:44 -0400

    Readjusted JSON format

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import requests
 
-url = "http://localhost:3000/posts/1"
+url = "http://localhost:3000/people"
 
 r = requests.get(url)
 
 r_json = r.json()
-
-def format(r_json):
-    print("ID: " + str(r_json['id']))
-    print("Title: " + str(r_json['title']))
-    print("Author: " + str(r_json['author']))
-
-format(r_json)
+print(r_json)
+print("-------------")
+print(r_json[0])
