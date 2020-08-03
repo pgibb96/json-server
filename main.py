a28983ff16ca35657de2456fed2aa3536fefbbd6
pgibb96 <pgibb96@gmail.com>
2020-08-03 07:46:10 -0400

    sorting JSON by interest

--- main.py
+++ main.py
@@ -4,7 +4,20 @@ url = "http://localhost:3000/people"
 
 r = requests.get(url)
 
+if r.status_code != 200:
+    sys.exit("GET error")
+
 r_json = r.json()
-print(r_json)
-print("-------------")
-print(r_json[0])
+
+def build_likes(r_json):
+    dictionary = {}
+    for person in r_json:
+        add_interest(person, dictionary)
+    return dictionary
+
+def add_interest(person, dictionary):
+    for interest in person['interests']:
+        # Storing list of people who have this interest
+        if interest not in dictionary:
+            dictionary[interest] = []
+        dictionary[interest].append(person['name'])
